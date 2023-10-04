2f2f84b457127c11bfe48aa99db1af19f11691b5
Ajay Thakur <ajayrajput3355@gmail.com>
2023-10-04 15:48:41 +0530

    Project and project type api changes

---
+++ server/models/project_type.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose'), { Schema } = mongoose,
+
+    projectCategorySchema = new Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: Boolean,
+            default : true
+        }
+    },
+        {
+            timestamps: true,
+        }
+    )
+
+module.exports = mongoose.model("project_type", projectCategorySchema);
