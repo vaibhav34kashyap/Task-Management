499393a9cab757172b8103f67e0d196fb8a43cfc
Ajay Thakur <ajayrajput3355@gmail.com>
2023-10-31 16:56:29 +0530

    User apis and token management

--- server/routes/milestone.route.js
+++ server/routes/milestone.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const milestoneRoute = express.Router();
 const milestone = require('../controller/milestone.controller');
+const { verifyAdmin } = require('../middleware/jwt.auth');
 
-milestoneRoute.post('/add', milestone.addMilestone);
-milestoneRoute.put('/update', milestone.updateMilestone);
-milestoneRoute.put('/updateStatus', milestone.updateStatus);
+milestoneRoute.post('/add', verifyAdmin, milestone.addMilestone);
+milestoneRoute.put('/update', verifyAdmin, milestone.updateMilestone);
+milestoneRoute.put('/updateStatus', verifyAdmin, milestone.updateStatus);
 milestoneRoute.get('/getMilestones', milestone.getMilestones);
 
 module.exports = milestoneRoute
--- server/routes/sprint.route.js
+++ server/routes/sprint.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const sprintRoute = express.Router();
 const path = require('../controller/sprint.controller');
+const { verifyAdmin } = require('../middleware/jwt.auth');
 
-sprintRoute.post('/add', path.addSprint);
-sprintRoute.put('/update', path.updateSprint);
-sprintRoute.put('/updateStatus', path.updateStatus);
+sprintRoute.post('/add', verifyAdmin, path.addSprint);
+sprintRoute.put('/update', verifyAdmin, path.updateSprint);
+sprintRoute.put('/updateStatus', verifyAdmin, path.updateStatus);
 sprintRoute.get('/getAMilestoneAllSprints', path.getAMilestoneAllSprints);
 
 
